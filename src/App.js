b16305503b351bdea51e89023a64fcd543cde306
erkanyilmaz <erkanyilmaz@msn.com>
2020-11-02 10:25:51 +0100

    first react project created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import AddUser from "./components/adduser";
+import Users from "./components/users";
+class App extends Component {
+  // state ={
+  //   users:[]
+  // };
+  constructor(props) {
+    super(props);
+      this.state={
+        users:[
+          {id:1,
+          name:"erkan",
+          age:45
+          },
+          {id:2,
+          name:"havva",
+          age:43
+          },
+          {id:3,
+            name:"nadir",
+            age:16
+          }
+        ]
+      };
+   }
+  render() {
+   
+    
+    return (
+      <div className="container">
+          <h4>User App</h4> 
+          <hr/>
+          <AddUser/>
+          <hr />
+          <Users />
+      </div>
+    );
+  }
 }
 
 export default App;
